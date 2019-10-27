b71f0e0208638b963f779d303c265e76152f3fae
Jakub Žitný <jakubzitny@avocode.com>
2019-10-27 04:04:11 +0100

    use webflow template with our mockups

--- pages/index.js
+++ pages/index.js
@@ -3,86 +3,123 @@ import Head from 'next/head'
 import Nav from '../components/nav'
 
 const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
+  <html data-wf-domain="spacetemplate.webflow.io" data-wf-page="52f134147711748d260004dc"
+      data-wf-site="52f134147711748d260004da">
 
-    <Nav />
+  <head>
+      <meta charset="utf-8" />
+      <title>Myelin Bracelet</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta content="Webflow" name="generator" />
+      <link href="styles.css" rel="stylesheet" type="text/css" />
+      <link href="https://y7v4p6k4.ssl.hwcdn.net/placeholder/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+      <link href="https://daks2k3a4ib2z.cloudfront.net/img/webclip.png" rel="apple-touch-icon" />
+      <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+      <script src="https://daks2k3a4ib2z.cloudfront.net/0globals/modernizr-2.7.1.js" type="text/javascript"></script>
+  </head>
 
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className='row'>
-        <a href='https://nextjs.org/docs' className='card'>
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href='https://nextjs.org/learn' className='card'>
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href='https://github.com/zeit/next.js/tree/master/examples'
-          className='card'
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+  <body>
+      <div className="navbar">
+          <div className="w-container">
+              <div className="w-row">
+                  <div className="left-nav w-col w-col-6 w-col-small-6 w-col-tiny-6">
+                      <a className="w-inline-block" href="/">
+                          <img className="logo"
+                              src="http://uploads.webflow.com/52f134147711748d260004da/52f175d20128ab830c0001c0_space-logo.svg"
+                              width="37" />
+                          <div className="brand">myelin bracelet</div>
+                      </a>
+                  </div>
+                  <div className="right-nav w-col w-col-6 w-col-small-6 w-col-tiny-6">
+                      <a className="social-btn w-inline-block" href="http://facebook.com/webflow">
+                          <img src="http://uploads.webflow.com/52f134147711748d260004da/52f158327711748d2600072b_facebook-icon_black.svg"
+                              width="25" /></a>
+                      <a className="social-btn w-inline-block" href="http://twitter.com/webflowapp">
+                          <img src="http://uploads.webflow.com/52f134147711748d260004da/52f158467711748d2600072c_twitter-icon_black.svg"
+                              width="25" /></a>
+                  </div>
+              </div>
+          </div>
       </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
+      <div className="hero section">
+          <div className="container w-container">
+              <h1>Welcome to Myelin,<br />your personal MS companion</h1>
+              <div className="sign-up-form w-form">
+                  <form className="w-clearfix" data-name="Signup Form" data-redirect="/success" name="wf-form-signup-form">
+                      <input className="field w-input" data-name="Email" maxlength="256" name="email"
+                          placeholder="Enter your email address" required="required" type="email" />
+                      <input className="button w-button" data-wait="Please wait..." type="submit" value="Get an invite" />
+                  </form>
+                  <div className="success-message w-form-done">
+                      <p>Thanks! You'll be the first to know.</p>
+                      <p className="spread-word">Help spread the word!</p>
+                      <div className="social-widget-wrapper w-clearfix">
+                          <div className="social-widget w-widget w-widget-twitter">
+                              <iframe
+                                  src="//platform.twitter.com/widgets/tweet_button.html#url=http%3A%2F%2Fwebflow.com&amp;counturl=webflow.com&amp;text=Check%20out%20this%20site&amp;count=horizontal&amp;size=m&amp;dnt=true"
+                                  scrolling="no" frameborder="0" allowtransparency="true"
+                                  style={{ border: "none", overflow: "hidden", width: "62px", height: "21px" }}></iframe>
+                              <div className="w-widget-twitter-count-shim">
+                                  <div className="w-widget-twitter-count-inner">...</div>
+                              </div>
+                          </div>
+                          <div className="social-widget w-widget w-widget-facebook">
+                              <iframe
+                                  src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2Fwebflow&amp;layout=button_count&amp;locale=en_US&amp;action=like&amp;show_faces=false&amp;share=false"
+                                  scrolling="no" frameborder="0" allowtransparency="true"
+                                  style={{ border: "none", overflow: "hidden", width: "90px", height: "20px" }}></iframe>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="error-message w-form-fail">
+                      <p>Oops! Something went wrong while submitting the form. :(</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div className="grey section" data-anchor="slide1">
+          <div className="w-container">
+              <h2 className="grey-heading">Ecosystem of bracelet, mobile app and data analytics</h2>
+              <p>Stress worsens the symptoms of MS and is related to relapses... </p>
+              <p>...Myelin bracelet measures stress over time, communicates it to others and to the user</p>
+              <img className="infinite"
+                  src="http://uploads.webflow.com/52f134147711748d260004da/52f184ea0128ab830c0002d2_infinite-loop.svg"
+                  width="187" />
+          </div>
+      </div>
+      <div className="section">
+          <div className="w-container">
+              <div>
+                  <img className="phone-image" sizes="(max-width: 479px) 80vw, 381px"
+                      src="x.svg"
+                      srcset="x.svg 911w" />
+                  <img className="phone-image" sizes="(max-width: 479px) 80vw, 381px"
+                      src="y.svg"
+                      srcset="y.svg 911w" />
+              </div>
+          </div>
+      </div>
+      <div className="grey section">
+          <div className="w-container">
+              <img className="footer logo"
+                  src="http://uploads.webflow.com/52f134147711748d260004da/52f175d20128ab830c0001c0_space-logo.svg"
+                  width="37" />
+              <a className="footer social-btn w-inline-block" href="http://twitter.com/webflowapp">
+                  <img
+                      src="http://uploads.webflow.com/52f134147711748d260004da/52f158467711748d2600072c_twitter-icon_black.svg" /></a>
+              <a className="footer social-btn w-inline-block" href="http://facebook.com/webflow">
+                  <img
+                      src="http://uploads.webflow.com/52f134147711748d260004da/52f158327711748d2600072b_facebook-icon_black.svg" /></a>
+              <a className="footer social-btn w-inline-block" href="mailto:contact@webflow.com">
+                  <img
+                      src="http://uploads.webflow.com/52f134147711748d260004da/52f17a7e1189e7810c00023a_email-icon-black.svg" /></a>
+          </div>
+      </div>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
+      <script src="https://daks2k3a4ib2z.cloudfront.net/52f134147711748d260004da/js/webflow.57f4ffb2a.js"
+          type="text/javascript"></script>
+  </body>
+  </html>
 )
 
 export default Home
